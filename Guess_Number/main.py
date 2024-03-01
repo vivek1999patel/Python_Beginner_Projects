9604af489da13bd252376be1cc3f084f2e9739fb
Vivek Patel <43326066+vivek1999patel@users.noreply.github.com>
2024-03-01 17:01:00 -0500

    Update Guess_Number
    Added computer_guess game as well

--- Guess_Number/main.py
+++ Guess_Number/main.py
@@ -11,5 +11,25 @@ def guess_number(x):
             print("Guess is samller than random number. Please try again!")
 
     print(f"Hurray, you've guessed it correctly! {random_number}")
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} is too low (L), or too high (H), or correct (C) : ").lower()
+        if feedback == "H":
+            high = guess - 1
+            print("It's too High")
+        elif feedback == "L":
+            low = guess + 1
+            print("It's too Low")
+
+    print(f"Hurray, you guess the correct number {guess}")
         
-guess_number(5)
+# guess_number(5)
+computer_guess(5)
