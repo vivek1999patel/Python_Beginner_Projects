2258cc51a18264d141cac07516fab4b8ac5f2094
Vivek Patel <43326066+vivek1999patel@users.noreply.github.com>
2024-03-03 16:07:45 -0500

    Updated Hangman

--- Hangman/main.py
+++ Hangman/main.py
@@ -4,12 +4,15 @@ def play():
     user = ''
     computer = random.choice(["Apple", "Kiwi", "Grapes"]).lower()
     line = draw_lines(computer)
+    used_letters = set()
+    print(line)
     points = 0
 
     while (''.join(line) != computer):
         if (points < 6):
             user = input("Guess the letter: ").lower()
-            is_True, line = complete_puzzle(line, computer, user)
+            used_letters.add(user)
+            is_True, line = complete_puzzle(line, computer, used_letters)
             print(line)
             if (is_True == False):
                 points += 1
@@ -22,16 +25,22 @@ def play():
 def draw_lines(word):
     line = []
     for letter in range(0, len(word)):
-        line.append("__")
+        line.append("-")
     return line
 
-def complete_puzzle(new_line, word, character):
-    if (character in word):
-        index = word.index(character)
-        new_line[index] = character
-        return True, new_line
-    
-    return False, new_line
+def complete_puzzle(line, word, used_letters):
+    # word_list = [letter if letter in used_letters else '-' for letter in word]
+    # Above is better way
+    word_list = line
+    for index,letter in enumerate(word):
+        if (letter in used_letters):
+            # print(word.index(letter))
+            print("letter is: " + letter + " at index: ", word.index(letter))
+            word_list[index] = letter
+        else:
+            word_list[index] = '-'
+
+    return True, word_list
 
 
 play()
